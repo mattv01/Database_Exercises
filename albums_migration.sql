0507491ee42cb5f7d17fd27bf1903619b0a15987
Matt Valdez <mattvaldez01@gmail.com>
2017-04-04 09:42:10 -0500

    created table called albums

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL DEFAULT 'UNKNOWN',
+    name  VARCHAR(100) NOT NULL DEFAULT 'UNKNOWN',
+    release_date YEAR,
+    sales DECIMAL(12, 2),
+    genre CHAR(255) NOT NULL DEFAULT 'UNKNOWN',
+    PRIMARY KEY (id)
+);
