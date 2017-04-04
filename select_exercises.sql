128166e251319cbc0b588ec395fa329dfa0e50c8
Matt Valdez <mattvaldez01@gmail.com>
2017-04-04 11:25:06 -0500

    put info message and output in same table box for more readability and so there isn't multiple boxes everywhere

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,19 +1,13 @@
 USE codeup_test_db;
 
-SELECT 'These are all the albums by Pink Floyd from this table.' AS 'Info';
-select name from albums where artist='Pink Floyd';
+select name 'These are all the albums by Pink Floyd from this table.' from albums where artist='Pink Floyd';
 
-SELECT "This is the year the album Sgt. Pepper's Lonely Hearts Club Band was released." AS 'Info';
-select release_date from albums where name='Sgt. Peppers Lonely Hearts Club Band';
+select release_date "This is the year the album Sgt. Pepper's Lonely Hearts Club Band was released." from albums where name='Sgt. Peppers Lonely Hearts Club Band';
 
-SELECT "This is the genre of the album 'Nevermind'." AS 'Info';
-select genre from albums where name='Nevermind';
+select genre "This is the genre of the album 'Nevermind'." from albums where name='Nevermind';
 
-SELECT "These are the albums that were released in the 1990's." AS 'Info';
-select name from albums where release_date BETWEEN 1990 AND 1999;
+select name "These are the albums that were released in the 1990's." from albums where release_date BETWEEN 1990 AND 1999;
 
-SELECT "These are the albums that had less than 20 million certified sales." AS 'Info';
-select name from albums where sales < 20000000;
+select name "These are the albums that had less than 20 million certified sales." from albums where sales < 20000000;
 
-SELECT "These are the albums that are in the genre 'Rock'." AS 'Info';
-select name from albums where genre='Rock';
+select name "These are the albums that are in the genre 'Rock'." from albums where genre='Rock';
